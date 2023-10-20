63026579f10f056367941de8d106e2eefd291b89
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-20 12:00:16 -0500

    added 3 paths

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,2 +1,13 @@
 class ExamplePagesController < ApplicationController
+  def welcome_method
+    render json: { message: "Welcome to Paradise!" }
+  end
+
+  def abandon_method
+    render json: { message: "Abandon hope all who enter here!" }
+  end
+
+  def purgatory_method
+    render json: { message: "Enjoy your stay in Purgatory!" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/Welcome_to_paradise!", controller: "example_pages", action: "welcome_method"
+  get "/Abandon_hope", controller: "example_pages", action: "abandon_method"
+  get "/come_hang", controller: "example_pages", action: "purgatory_method"
 end
